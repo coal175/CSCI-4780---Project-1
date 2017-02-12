8c4c0e39a8c5d938d61961f8cba2b584e321cc0b
Kyle Mercer <kyle.mercer.km@gmail.com>
2017-02-12 16:09:09 -0500

    updated client

--- src/client.java
+++ src/client.java
@@ -1,7 +1,11 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -9,22 +13,45 @@ public class client {
 
 	public static void main(String[] args) throws Exception{
 		
-		File file = new File(".");
-		System.out.println(file.getAbsolutePath());
-		
 		Socket socket = new Socket("localhost", 5000);
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		//DataInputStream ds = new DataInputStream(socket.getInputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		for(int i = 0; i < 3; i++){
 			System.out.print("> ");
 			String s = scanner.nextLine();
-			System.out.println("sending");
 			
 			out.writeBytes(s + "\n");
 			
-			//receive output
+			//receive output, check if receiving any file
+			String basicInput = in.readLine();
+			String[] input = basicInput.split(" ");
+			if(input[0].equals("GET")){
+				System.out.println("getting ready to write file");
+				File file = new File(input[1]);
+				FileOutputStream fos = new FileOutputStream(file);
+				
+				char[] bytes = new char[Integer.parseInt(input[2])];
+				//while(true){
+				
+					in.read(bytes);
+					//if(bytes == null){
+						//break;
+					//}
+					fos.write(new String(bytes).getBytes());
+				//}
+				fos.close();
+				
+				System.out.println("finished writing");
+				
+			}else{
+				System.out.println(basicInput);
+			}
+			
+			
 		}
 		
 		out.close();
