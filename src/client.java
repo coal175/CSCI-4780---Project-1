fb8503bd4dc0a99722ec61e02a82e9f7af207d44
Kyle Mercer <kyle.mercer.km@gmail.com>
2017-02-13 02:23:24 -0500

    completed put, ls, cd and pwd

--- src/client.java
+++ src/client.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -13,49 +14,73 @@ public class client {
 
 	public static void main(String[] args) throws Exception{
 		
+		File cwd = new File("C:\\Users\\Kylem\\workspace\\server");
+		
 		Socket socket = new Socket("localhost", 5000);
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		//DataInputStream ds = new DataInputStream(socket.getInputStream());
-		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		DataOutputStream outServer = new DataOutputStream(socket.getOutputStream());
+		BufferedReader inServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		for(int i = 0; i < 3; i++){
+		for(int i = 0; i < 15; i++){
 			System.out.print("> ");
 			String s = scanner.nextLine();
 			
-			out.writeBytes(s + "\n");
+			//check to see if put command is used.   if yes, then add on metadata and send command
+			String[] inputArray = s.split(" ");
+			if(inputArray[0].equals("put") && inputArray.length == 2){
+				
+				//check if file exists
+				File outFile = new File(cwd.getAbsolutePath() + "\\" + inputArray[1]);
+				if(!outFile.exists()){
+					System.out.println(inputArray[1] + " does not exist");
+					continue;
+				}
+				
+				byte[] bytes = new byte[(int) outFile.length()];
+				FileInputStream fs = new FileInputStream(outFile);
+				fs.read(bytes);
+				fs.close();
+				
+				outServer.writeBytes("PUT " + inputArray[1] + " " + bytes.length + "\n"); //tell server to prepare to receive file
+				outServer.write(bytes, 0, bytes.length); //This could probably be simplifed to just outServer.write(bytes)
+				
+				System.out.println("finished sending file");
+				continue;
+			}
 			
-			//receive output, check if receiving any file
-			String basicInput = in.readLine();
+			outServer.writeBytes(s + "\n");
+			
+			//receive output and handle it based on command
+			String basicInput = inServer.readLine();
 			String[] input = basicInput.split(" ");
+			
 			if(input[0].equals("GET")){
 				System.out.println("getting ready to write file");
+				
 				File file = new File(input[1]);
 				FileOutputStream fos = new FileOutputStream(file);
 				
 				char[] bytes = new char[Integer.parseInt(input[2])];
-				//while(true){
-				
-					in.read(bytes);
-					//if(bytes == null){
-						//break;
-					//}
-					fos.write(new String(bytes).getBytes());
-				//}
+				inServer.read(bytes);
+
+				fos.write(new String(bytes).getBytes());
 				fos.close();
 				
 				System.out.println("finished writing");
-				
+			}if(inputArray[0].equals("ls") && inputArray.length ==1){
+				//fix formatting
+				String[] listings = input[0].split("\\?");
+				for(int k = 0; k < listings.length; k++){
+					System.out.println(listings[k]);
+				}
 			}else{
 				System.out.println(basicInput);
 			}
-			
-			
 		}
 		
-		out.close();
-		
+		outServer.close();
+		scanner.close();
 		socket.close();
 	}
 
