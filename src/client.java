c5e141606f95bedfc8c190631b1dea6c371c9527
coal175 <cgibbon12@gmail.com>
2017-02-16 10:35:14 -0500

    Command line args and multi-threading
    Client now requires a command line argument (port number) and the
    server requires 2 command line arguments (address and port number).
    
    Created ServerThread class which extends Runnable to be run inside the
    threads created in Server.java.  This is now where the loop method so
    that each thread can run its own loop.

--- src/client.java
+++ src/client.java
@@ -11,24 +11,29 @@ import java.util.Scanner;
 public class client {
 
 	public static void main(String[] args) throws Exception{
-		
+
+		if (args.length != 2) {
+			System.out.println("Invalid format.  Try: java client 'address' 'port#'");
+			return;
+		}
+
 		File cwd = new File(".");
 		cwd = cwd.getAbsoluteFile().getParentFile();
-		
-		Socket socket = new Socket("localhost", 5000);
+
+		Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
 		DataOutputStream outServer = new DataOutputStream(socket.getOutputStream());
 		BufferedReader inServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		while(true){
 		    System.out.print("> ");
 		    String s = scanner.nextLine();
-		    
+
 		    //check to see if put command is used.   if yes, then add on metadata and send command
 		    String[] inputArray = s.split(" ");
 		    if(inputArray[0].equals("put") && inputArray.length == 2){
-			
+
 			//check if file exists
 			File outFile = new File(cwd.getAbsolutePath() + "/" + inputArray[1].trim());
 			System.out.println(outFile.getAbsolutePath());
@@ -36,15 +41,15 @@ public class client {
 			    System.out.println(inputArray[1] + " does not exist");
 			    continue;
 			}
-			
+
 			byte[] bytes = new byte[(int) outFile.length()];
 			FileInputStream fs = new FileInputStream(outFile);
 			fs.read(bytes);
 			fs.close();
-			
+
 			outServer.writeBytes("PUT " + inputArray[1] + " " + bytes.length + "\n"); //tell server to prepare to receive file
 			outServer.write(bytes, 0, bytes.length); //This could probably be simplifed to just outServer.write(bytes)
-			
+
 			System.out.println("finished sending file");
 			continue;
 		    }else if(inputArray[0].equals("quit")){
@@ -52,25 +57,25 @@ public class client {
 			socket.close();
 			break;
 		    }
-		    
+
 		    outServer.writeBytes(s + "\n");
-		    
+
 		    //receive output and handle it based on command
 		    String basicInput = inServer.readLine();
 		    String[] input = basicInput.split(" ");
-		    
+
 		    if(input[0].equals("GET")){
 			System.out.println("getting ready to write file");
-			
+
 			File file = new File(input[1]);
 			FileOutputStream fos = new FileOutputStream(file);
-			
+
 			char[] bytes = new char[Integer.parseInt(input[2])];
 			inServer.read(bytes);
-			
+
 			fos.write(new String(bytes).getBytes());
 			fos.close();
-			
+
 			System.out.println("finished writing");
 		    }if(inputArray[0].equals("ls") && inputArray.length ==1){
 			//fix formatting
@@ -82,10 +87,10 @@ public class client {
 			System.out.println(basicInput);
 		    }
 		}
-		
+
 		outServer.close();
 		scanner.close();
 		socket.close();
 	}
-    
+
 }
